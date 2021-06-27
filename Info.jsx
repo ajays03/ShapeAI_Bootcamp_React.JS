8704df30a5fefdf8073acec036229ab4ce256a74
ajays03 <83328607+ajays03@users.noreply.github.com>
2021-06-27 20:34:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p> Basic Web dev React Js Bootcamp.This was a amazing bootcamp taken up by shaurya sinha We covered everything from scratch including Javascript React.js,HTML.</p>
+    </div>
+  );
+}
+
+export default Info;
